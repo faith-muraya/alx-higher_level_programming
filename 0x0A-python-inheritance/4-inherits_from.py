b2e42c0d0204f2140002078fe1b0a5114ef38c45
faith-muraya <faith.muraya04@gmail.com>
2023-01-09 01:24:46 -0800

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,9 +4,9 @@
 
 
 def is_kind_of_class(obj, a_class):
-    '''function: is_kind_of_class
+    '''the object is an instance of a class that inherited (directly or indirectly)
     obj: an object
     a_class: a class
-    Returns: Bool
+    returns None
     '''
-    return isinstance(obj, a_class)
+    return type(obj) != a_class and isinstance(obj, a_class)
